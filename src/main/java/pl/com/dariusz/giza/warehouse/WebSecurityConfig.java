2c9d037e38adbeb3c37a723ce7cf28b619e81061
darek-giza <darek_giza@op.pl>
2020-01-19 10:53:06 +0100

    service login repair

--- src/main/java/pl/com/dariusz/giza/warehouse/WebSecurityConfig.java
+++ src/main/java/pl/com/dariusz/giza/warehouse/WebSecurityConfig.java
@@ -1,10 +1,13 @@
 package pl.com.dariusz.giza.warehouse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 
 @Configuration
@@ -14,7 +17,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     public WebSecurityConfig(UserDetailsServiceImpl userDetailsServiceimpl) {
-       this.userDetailsServiceimpl=userDetailsServiceimpl;
+        this.userDetailsServiceimpl = userDetailsServiceimpl;
     }
 
 
@@ -26,9 +29,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/api/users").permitAll()
+                .antMatchers("/api/users").hasAnyRole("USER", "ADMIN")
                 .antMatchers("/login").permitAll()
                 .and()
-                .formLogin();
+                .formLogin()
+                .and().csrf().disable();
     }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 }
