9af82d819d7e796999f25f54f217374eee846405
darek-giza <darek_giza@op.pl>
2020-03-26 11:42:00 +0100

    Change in income collection method

--- src/main/java/pl/com/dariusz/giza/financeTracker/repositories/IncomeRepository.java
+++ src/main/java/pl/com/dariusz/giza/financeTracker/repositories/IncomeRepository.java
@@ -3,6 +3,10 @@ package pl.com.dariusz.giza.financeTracker.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.com.dariusz.giza.financeTracker.domain.budgets.Income;
 
+import java.util.List;
+
 public interface IncomeRepository extends JpaRepository<Income, Long> {
+
+    List<Income> findByBudget_Id(Long id);
 }
 
--- src/main/java/pl/com/dariusz/giza/financeTracker/service/Income/IncomeServiceImpl.java
+++ src/main/java/pl/com/dariusz/giza/financeTracker/service/Income/IncomeServiceImpl.java
@@ -26,12 +26,12 @@ public class IncomeServiceImpl implements IncomeService {
 
     @Override
     public List<Income> getUserIncomes(Budget budget) {
-        final List<Income> allIncomes = incomeRepository.findAll();
         final Long idUser = budget.getId();
 
-        return allIncomes.stream()
-                .filter(e -> e.getBudget().getId() == idUser)
-                .sorted((o1, o2) -> o2.getDate().compareTo(o1.getDate()))
+        final List<Income> userIncome = incomeRepository.findByBudget_Id(idUser);
+
+        return userIncome.stream()
+                .sorted((o1,o2)->o2.getDate().compareTo(o1.getDate()))
                 .collect(Collectors.toList());
     }
 
