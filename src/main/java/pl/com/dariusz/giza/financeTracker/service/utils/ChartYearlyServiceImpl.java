5bb37a7c0a6a1d1ddcedc238624c3dad5cfb3de2
darek-giza <darek_giza@op.pl>
2020-04-07 17:15:47 +0200

    Fix yearly chart

--- src/main/java/pl/com/dariusz/giza/financeTracker/service/utils/ChartYearlyServiceImpl.java
+++ src/main/java/pl/com/dariusz/giza/financeTracker/service/utils/ChartYearlyServiceImpl.java
@@ -47,15 +47,26 @@ public class ChartYearlyServiceImpl implements ChartYearlyService {
     public ChartYearly fillChartYearly(Budget budget, Integer month) {
         BigDecimal incomes = reduceMonthlyIncomes(budget, month);
         BigDecimal expenses = reduceMonthlyExpenses(budget, month);
+        BigDecimal lastMonthIncomes = reduceMonthlyIncomes(budget, month-1);
+        BigDecimal lastMonthExpenses = reduceMonthlyExpenses(budget, month-1);
 
-        BigDecimal budgetMonthly = incomes.subtract(expenses);
+        if (month == 1) {
+
+            final BigDecimal initial = initialBudget(budget);
+
+            final BigDecimal budgetMonthly = initial.add(incomes.subtract(expenses));
+
+            return new ChartYearly(getMonth(month),incomes,expenses,budgetMonthly);
+        }
+        final BigDecimal lastMonth = lastMonthIncomes.subtract(lastMonthExpenses);
+
+        BigDecimal budgetMonthly=lastMonth.add(incomes.subtract(expenses));
 
         return new ChartYearly(getMonth(month), incomes, expenses, budgetMonthly);
     }
 
     public BigDecimal reduceMonthlyIncomes(Budget budget, Integer month) {
-        final List<Income> incomes = incomeRepository.findByBudget_Id(budget.getId());
-        return incomes.stream().filter(e -> e.getDate().getYear() == now().getYear())
+        return getIncomes(budget).stream().filter(e -> e.getDate().getYear() == now().getYear())
                 .filter(e -> e.getDate().getMonth().getValue() == month)
                 .map(Income::getAmount)
                 .reduce(BigDecimal::add)
@@ -63,12 +74,35 @@ public class ChartYearlyServiceImpl implements ChartYearlyService {
     }
 
     public BigDecimal reduceMonthlyExpenses(Budget budget, Integer month) {
-        final List<Expense> expense = expenseRepository.getByBudget_Id(budget.getId());
-        return expense.stream().filter(e -> e.getDate().getYear() == now().getYear())
+        return getExpenses(budget).stream().filter(e -> e.getDate().getYear() == now().getYear())
                 .filter(e -> e.getDate().getMonth().getValue() == month)
                 .map(e -> e.getAmount())
                 .reduce(BigDecimal::add)
                 .orElse(BigDecimal.ZERO);
+    }
+
+    public BigDecimal initialBudget(Budget budget){
+
+        final BigDecimal expenseValue = getExpenses(budget).stream()
+                .filter(e -> e.getDate().getYear() == now().getYear() - 1)
+                .map(e -> e.getAmount())
+                .reduce(BigDecimal::add)
+                .orElse(BigDecimal.ZERO);
+
+        final BigDecimal incomesValue = getIncomes(budget).stream()
+                .filter(e -> e.getDate().getYear() == now().getYear() - 1)
+                .map(e -> e.getAmount())
+                .reduce(BigDecimal::add)
+                .orElse(BigDecimal.ZERO);
+
+        return incomesValue.subtract(expenseValue);
+    }
+
+    public List<Expense> getExpenses(Budget budget){
+        return expenseRepository.getByBudget_Id(budget.getId());
+    }
+    public List<Income> getIncomes(Budget budget){
+        return incomeRepository.findByBudget_Id(budget.getId());
     }
 
     public LocalDate now() {
