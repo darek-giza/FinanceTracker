6b903f3b5a2ea7bc7d4bde96b8d8bd2eb8d5d26a
darek-giza <darek_giza@op.pl>
2020-08-03 23:37:40 +0200

    Add support for jwt token to swagger-ui

--- src/main/java/pl/com/dariusz/giza/financeTracker/config/SwaggerConfig.java
+++ src/main/java/pl/com/dariusz/giza/financeTracker/config/SwaggerConfig.java
@@ -1,28 +1,41 @@
 package pl.com.dariusz.giza.financeTracker.config;
 
+import com.google.common.collect.Lists;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.ResponseEntity;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
 
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+
     @Bean
     public Docket get() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
+        Docket docket = new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(createApiInfo())
+                .forCodeGeneration(true)
+                .genericModelSubstitutes(ResponseEntity.class)
+                .securityContexts(Lists.newArrayList(securityContext()))
+                .securitySchemes(Arrays.asList(apiKey()))
+                .useDefaultResponseMessages(false);
+
+        return docket.select()
                 .paths(PathSelectors.any())
                 .apis(RequestHandlerSelectors.any())
-                .build().apiInfo(createApiInfo());
+                .build();
     }
 
     private ApiInfo createApiInfo() {
@@ -34,6 +47,27 @@ public class SwaggerConfig {
                 "my own licence", null,
                 Collections.emptyList()
         );
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("JWT", AUTHORIZATION_HEADER, "header");
+    }
+
+    private SecurityContext securityContext() {
+        final SecurityContext build = SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .forPaths(PathSelectors.any())
+                .build();
+        return build;
+    }
+
+    ArrayList<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope
+                = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Lists.newArrayList(
+                new SecurityReference("JWT", authorizationScopes));
     }
 
 }
